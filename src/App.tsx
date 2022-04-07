7ac7e9d2a62b1440f004dbf7556966f7f12fab45
vasylsemak <vasylsmk1401@gmial.com>
2022-04-07 13:50:26 -0700

    implement interface, optional property of TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+interface AppProps {
+  headerText: string;
+  extraText?: string;
+}
+
+function App({ headerText, extraText='default text' }: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{headerText}</h1>
+      {extraText && <p>{extraText}</p>}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App headerText='This is a Header' extraText='some extra text' />
   </React.StrictMode>,
   document.getElementById('root')
 );
